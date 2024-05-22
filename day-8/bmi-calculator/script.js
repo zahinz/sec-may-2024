6f067032cef9008bc79be24f77facf160b375c8d
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-05-22 21:40:06 +0800

    insert assignment day 8

---
+++ day-8/bmi-calculator/script.js
@@ -0,0 +1,25 @@
+//  this is an example of pseudocode
+//  pseudocode is a way of planning out your code before you write it
+
+// BMI calculator web app
+// BMI is calculated by dividing a person's weight in kilograms by the square of his/her height in meters
+// BMI = kg/m^2
+// BMI = kg/m/m
+// BMI = kg/(m*m)
+
+// category	BMI (kg/m2)
+// 1. Very severely underweight	< 15
+// 2. Severely underweight	< 16
+// 3. Underweight	< 18.5
+// 4. Normal (healthy weight)	18.5 - 25
+// 5. Overweight	25 - 30
+// 6. Obese Class I (Moderately obese)	30 - 35
+// 7. Obese Class II (Severely obese)	35 - 40
+// 8. Obese Class III (Very severely obese)	> 40
+
+// how our app will work:
+// input user for weight in kilograms
+// input user for height in meters
+// calculate BMI based on user input (weight and height)
+// display BMI at h2 with id="bmiValue"
+// display BMI category at h2 with id="bmiCategory"
