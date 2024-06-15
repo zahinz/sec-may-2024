aba9eb69b05984e0d9e0d6547d9fe6774d487c0c
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-06-15 20:47:53 +0800

    feat: Add new HTML pages for 404, about, and contact
    This commit adds new HTML pages for the 404 error page, the about page, and the contact page. These pages provide relevant content and improve the overall user experience of the website.

--- day-14/http-module/index.js
+++ day-14/http-module/index.js
@@ -1,24 +1,35 @@
 const http = require("node:http");
 const fs = require("node:fs");
 
-// function declaration
-// function add(a, b) {
-//   return a + b;
-// }
-
-// function expression or arrow function
-// const add = (a, b) => {
-//   return a + b;
-// };
-
 const indexHtml = fs.readFileSync("./index.html", "utf-8");
+const aboutHtml = fs.readFileSync("./about.html", "utf-8");
+const contactHtml = fs.readFileSync("./contact.html", "utf-8");
+const notFoundHtml = fs.readFileSync("./404.html", "utf-8");
 // Create a local server to receive data from
 const server = http.createServer(function (req, res) {
+  // handle a multiple routes in node.js HTTP server
   const url = req.url;
-  console.log(url);
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(indexHtml);
-  res.end();
+  console.log("URL", url);
+  if (url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "X-Custom-Secret": "Hello, this is password for you",
+    });
+    res.write(indexHtml);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(aboutHtml);
+    res.end();
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(contactHtml);
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write(notFoundHtml);
+    res.end();
+  }
 });
 
 // listen server on port 8888
