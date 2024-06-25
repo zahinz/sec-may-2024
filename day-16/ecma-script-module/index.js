f98a7260d95562593d177dcd2dec7c9f25d20d6d
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-06-25 20:20:52 +0800

    feat: Add ECMAScript module support
    This commit adds support for ECMAScript modules to the project. It introduces a new JavaScript file, `index.js`, which imports the `math.js` module using the `import` statement. The `math.js` module exports two functions, `add` and `subtract`, which are used to perform mathematical operations in the `index.js` file. This update improves the code organization and allows for better code reuse and maintainability.

---
+++ day-16/ecma-script-module/index.js
@@ -0,0 +1,8 @@
+// const math = require("./math.js");
+import math from "./math.js";
+
+const addAnswer = math.add(16, 26);
+const subAnswer = math.subtract(16, 26);
+
+console.log(addAnswer);
+console.log(subAnswer);
---
+++ day-16/ecma-script-module/math.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+const math = {
+  add,
+  subtract,
+};
+
+// module.exports = math;
+export default math;
