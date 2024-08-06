b37724e1540457218ab75b7d3a3effd209f63795
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-08-06 22:00:43 +0800

    finish react hook form

--- react-day-3/react-form/src/pages/FormC.jsx
+++ react-day-3/react-form/src/pages/FormC.jsx
@@ -1,10 +1,64 @@
 import Header from "../components/Header";
+import { formContainerStyle, inputContainer } from "./FormA";
 
 function FormC() {
+  function handleSubmit(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    const data = {
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(data);
+  }
   return (
     <div>
       <Header />
       <h1>Form C</h1>
+      <form onSubmit={handleSubmit} style={formContainerStyle}>
+        <h4>Register</h4>
+        <div style={inputContainer}>
+          <label htmlFor="name">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Insert your email"
+          />
+        </div>
+        <div style={inputContainer}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Insert your password"
+          />
+        </div>
+        <div style={inputContainer}>
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirm your password"
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
--- react-day-3/react-form/src/pages/FormD.jsx
+++ react-day-3/react-form/src/pages/FormD.jsx
@@ -1,10 +1,63 @@
 import Header from "../components/Header";
+import { formContainerStyle, inputContainer } from "./FormA";
+import { useForm } from "react-hook-form";
 
 function FormD() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(values) {
+    if (values.password !== values.confirmPassword) {
+      alert("Password and Confirm Password must be the same");
+      return;
+    }
+    alert("Form submitted");
+  }
+
+  console.log(errors);
   return (
     <div>
       <Header />
       <h1>Form D</h1>
+      <form onSubmit={handleSubmit(onSubmit)} style={formContainerStyle}>
+        <h4>Register</h4>
+        <div style={inputContainer}>
+          <label htmlFor="name">Email</label>
+          <input
+            {...register("email", {
+              required: "Email is required",
+            })}
+            type="email"
+            placeholder="Insert your email"
+          />
+          <span>{errors?.email?.message}</span>
+        </div>
+        <div style={inputContainer}>
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", {
+              required: "Password is required",
+            })}
+            type="password"
+            placeholder="Insert your password"
+          />
+          <span>{errors?.password?.message}</span>
+        </div>
+        <div style={inputContainer}>
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            {...register("confirmPassword", {
+              required: "Confirm Password is required",
+            })}
+            type="password"
+            placeholder="Confirm your password"
+          />
+          <span>{errors?.confirmPassword?.message}</span>
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
