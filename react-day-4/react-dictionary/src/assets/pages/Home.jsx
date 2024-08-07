9396c732553b2cfacd51805ff6a48e26e1bf742e
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-08-07 21:34:47 +0800

    finish react lifecycle

--- react-day-4/react-dictionary/src/assets/pages/Home.jsx
+++ react-day-4/react-dictionary/src/assets/pages/Home.jsx
@@ -1,12 +1,50 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+  // for the best practive state always use constant to store the value of the state
+  const [counter, setCounter] = useState(0);
+
+  // increment is achieved by setting the counter to the current value of the counter plus 1
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
+  // create a function which monitor the counter value, whenever the counter value more the 10, alert a message to the user that the counter value is more than 10
+  // useEffect(a,b)
+  // a: function to run when the state changes
+  // b: state to run the function is form of array
+  useEffect(
+    function () {
+      if (counter === 10) {
+        //   mount react lifecycle
+        alert("10 mounted");
+        //   unmount react lifecycle
+        return function () {
+          alert("10 unmounted");
+        };
+      }
+    },
+    [counter]
+  );
   return (
     <div>
       <h1>Home</h1>
       <hr />
       <div>
-        <h2>1</h2>
-        <button>-</button>
-        <button>+</button>
+        <p>Counter</p>
+        <h2>{counter}</h2>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+        <div>
+          <label for="file">Downloading progress:</label>
+          <progress id="file" value={counter} max="100">
+            {counter}%
+          </progress>
+        </div>
       </div>
     </div>
   );
